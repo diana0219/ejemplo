f63ddf5cb8671003b4a57c0fb30b3733be7ec0d8
diana0219 <dcsanabria71@misena.edu.co>
2021-03-14 14:10:31 -0500

    ejemplo de error

---
+++ ejercicio_tablamultiplicar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_tablamultiplicar
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            int res;//variable para guardar el resultado de la multiplicación
+            for (int i = 1; i <= 10; i++)//ciclo for para indicar cada tabla
+            {
+                Console.WriteLine("     Tabla del " + i);//se indica la tabla en que está
+                for (int j = 1; j <= 10; j++)//ciclo for para calcular una tabla
+                {
+                    res = j * i;//resultado es igual a j multiplicado por i
+                    Console.WriteLine(+j + " x " + i + " = " + res);//se escribe la operación en pantalla ejemplo  1 x 5 = 5
+                }
+            }
+            Console.ReadLine();//se detiene pantalla
+        }
+    }
+}
+	
+
